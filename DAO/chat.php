79a911784557bb0d8ede8308cb3aa4f533a15669
Felipe <contatofemachado@gmail.com>
2023-06-14 20:52:04 -0300

    ne

--- DAO/chat.php
+++ DAO/chat.php
@@ -1,15 +1,36 @@
 <?php
 require('conexao.php');
 //expurgo da mensagem 50 atras
+$query = "SELECT * FROM chat ORDER BY id DESC LIMIT 1 OFFSET 49";
+$result = $conn->query($query);
+if ($result->num_rows > 0) {
+    // Obtém o ID do registro a ser deletado
+    $row = $result->fetch_assoc();
+    $idToDelete = $row['id'];
 
-
-$sql = "SELECT * FROM chat;";
-$result = mysqli_query($strcon, $sql);
-while ($row = mysqli_fetch_array($result)) :; ?>
-    <div id="msgs" class="bg-white m-1 text-break">
-        <h4 class="text-dark m-1" id="sender"><?php echo $row['msgFrom'] ?></h4>
-        <p class="text-dark m-2" id="message"><?php echo $row['msg'] ?></p>
-    </div>
+    // Excluindo o registro
+    $deleteQuery = "DELETE FROM chat WHERE id = $idToDelete";
+    if ($conn->query($deleteQuery) == TRUE) {
+        $sql = "SELECT * FROM chat;";
+        $result = mysqli_query($strcon, $sql);
+        while ($row = mysqli_fetch_array($result)) :; ?>
+            <div id="msgs" class="bg-white m-1 text-break">
+                <h4 class="text-dark m-1" id="sender"><?php echo $row['msgFrom'] ?></h4>
+                <p class="text-dark m-2" id="message"><?php echo $row['msg'] ?></p>
+            </div>
+        <?php endwhile;
+    } else {
+        echo "Erro ao excluir o registro: " . $conn->error;
+    }
+} else {
+    $sql = "SELECT * FROM chat;";
+    $result = mysqli_query($strcon, $sql);
+    while ($row = mysqli_fetch_array($result)) :; ?>
+        <div id="msgs" class="bg-white m-1 text-break">
+            <h4 class="text-dark m-1" id="sender"><?php echo $row['msgFrom'] ?></h4>
+            <p class="text-dark m-2" id="message"><?php echo $row['msg'] ?></p>
+        </div>
 <?php endwhile;
-
+    echo "Não foi encontrado o último quinquagésimo registro na tabela.";
+}
 ?>
