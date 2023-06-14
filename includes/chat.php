a7b4c348f08d9387e612fbac8a82897b0ac8041b
Felipe <contatofemachado@gmail.com>
2023-06-14 18:56:43 -0300

    css adjust

--- includes/chat.php
+++ includes/chat.php
@@ -8,21 +8,19 @@
             </div>
             <form id="chatz" class="form-container">
                 <div class="modal-body">
-                    <h1>Chat</h1>
-                    <p>O chat se atualiza sozinho a cada 10 segundos</p>
-                    <div id="corpoChat" class="blocoMsg w-100 p-1 overflow-auto" style="background-color: #eee; height: 400px; margin-bottom: 10px;">
-                        <?php
-                        require 'DAO/chat.php';
-                        ?>
+                    <div class="col">
+                        <h1>Chat</h1>
+                        <p>O chat se atualiza sozinho a cada 10 segundos</p>
+                        <div id="corpoChat" class="overflow-auto" style="background-color: #eee; height: auto; margin-bottom: 10px;">
+                            <?php
+                            require 'DAO/chat.php';
+                            ?>
+                        </div>
+                        <input type="hidden" id="nomeUsu" name="nomeUsu" value="<?php echo $_SESSION['usuario']; ?>"></input>
+                        <input type="hidden" id="idUsu" name="idUsu" value="<?php echo $_SESSION['id']; ?>"></input>
+                        <input class="mb-1 w-100" placeholder="Digite sua mensagem" type="text" id="msgText" name="msgText">
+                        <button type="submit" class="btn">Enviar</button>
                     </div>
-                    <input type="hidden" id="nomeUsu" name="nomeUsu" value="<?php echo $_SESSION['usuario']; ?>"></input>
-                    <input type="hidden" id="idUsu" name="idUsu" value="<?php echo $_SESSION['id']; ?>"></input>
-                    <input class="mb-1 w-100" placeholder="Digite sua mensagem" type="text" id="msgText" name="msgText">
-                    <button type="submit" class="btn">Enviar</button>
-
-                </div>
-                <div class="modal-footer">
-                    <button class="btnar  btn-secondary" type="button" data-dismiss="modal">fechar</button>
                 </div>
             </form>
         </div>
