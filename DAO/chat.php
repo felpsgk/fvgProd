f97edd6d4dc11aa2aa1fd6325e594b775540aba8
Felipe <contatofemachado@gmail.com>
2023-06-14 20:50:31 -0300

    a

--- DAO/chat.php
+++ DAO/chat.php
@@ -1,23 +1,7 @@
 <?php
 require('conexao.php');
 //expurgo da mensagem 50 atras
-$query = "SELECT * FROM chat ORDER BY id DESC LIMIT 1 OFFSET 49";
-$result = $conn->query($query);
-if ($result->num_rows > 0) {
-    // Obtém o ID do registro a ser deletado
-    $row = $result->fetch_assoc();
-    $idToDelete = $row['id'];
 
-    // Excluindo o registro
-    $deleteQuery = "DELETE FROM chat WHERE id = $idToDelete";
-    if ($conn->query($deleteQuery) == TRUE) {
-        echo "O último quinquagésimo registro foi excluído com sucesso!";
-    } else {
-        echo "Erro ao excluir o registro: " . $conn->error;
-    }
-} else {
-    echo "Não foi encontrado o último quinquagésimo registro na tabela.";
-}
 
 $sql = "SELECT * FROM chat;";
 $result = mysqli_query($strcon, $sql);
