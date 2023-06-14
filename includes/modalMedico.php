47b0a6ae4c37137b04fbffa9d747cb7bc168c36e
Felipe <contatofemachado@gmail.com>
2023-06-14 19:15:27 -0300

    modal

--- includes/modalMedico.php
+++ includes/modalMedico.php
@@ -3,17 +3,19 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="medicoCadModalLabel">Cadastrar novo médico</h1>
-                <button class="btn btn-close btn-secondary" type="button" data-dismiss="modal"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
+
                 <div class="form-floating mb-3">
-                    <input onchange="formatarCRM()" maxlength="5" type="text" required="required" class="form-control" id="crmModal" name="crmModal" placeholder="CRM do médico">
+                    <input type="text" class="form-control" id="crmModal" name="crmModal" placeholder="CRM do médico" onchange="formatarCRM()" required="required" maxlength="5">
                     <label for="crmModal">CRM do médico (apenas números)</label>
                 </div>
                 <div class="form-floating">
-                    <input type="text" class="form-control" required="required" id="nome" placeholder="Nome do médico">
+                    <input type="text" class="form-control" id="nome" placeholder="Nome do médico" required="required">
                     <label for="nome">Nome do médico</label>
                 </div>
+
             </div>
             <div class="modal-footer">
                 <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Cancelar</button>
@@ -22,26 +24,6 @@
         </div>
     </div>
 </div>
-<!--
-<div class="modal fade" id="medicoCadModal" tabindex="-1" role="dialog" aria-labelledby="modalCadMedicoLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalCadMedicoLabel">Cadastrar novo médico</h5>
-                <button class="btn btn-close btn-secondary" type="button" data-dismiss="modal"></button>
-            </div>
-            <form action="DAO/medicoDAO.php" method="POST">
-                <div class="modal-body">
-
-                </div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
-                    <button type="submit" name="inserirmedico" class="btn btn-success">Salvar</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>-->
 <script>
     function formatarCRM() {
         var crmInput = document.getElementById('crmModal');
