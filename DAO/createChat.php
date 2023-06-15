cc959a0617b93fd24db629269dd98dde1fd7dfc6
Felipe <contatofemachado@gmail.com>
2023-06-14 21:00:43 -0300

    set hour

--- DAO/createChat.php
+++ DAO/createChat.php
@@ -4,13 +4,16 @@ require '../conexao.php';
 $msg = $_POST['msgText'];
 $msgFrom = $_POST['nomeUsu'];
 $idUsu = $_POST['idUsu'];
+$horaAtual = $_POST['horaAtual'];
 
 $data = array(
     ':msg'  => $_POST["msgText"],
     ':msgFrom'  => $_POST["nomeUsu"],
-    ':idUsu'  => $_POST["idUsu"]
+    ':idUsu'  => $_POST["idUsu"],
+    ':horaAtual'  => $_POST["horaAtual"]
 );
-$sqlold = "INSERT INTO chat (userId, msg, msgFrom) VALUES (:idUsu, :msg,:msgFrom)";
+$sqlold = "INSERT INTO chat (userId, msg, msgFrom, horaAtual) 
+VALUES (:idUsu, :msg,:msgFrom,:horaAtual)";
 
 $statement = $connect->prepare($sqlold);
 
@@ -18,9 +21,8 @@ if ($statement->execute($data)) {
     $output = array(
         'msg'  => $_POST["msgText"],
         'msgFrom'  => $_POST["nomeUsu"],
-        'idUsu'  => $_POST["idUsu"]
+        'idUsu'  => $_POST["idUsu"],
+        'horaAtual'  => $_POST["horaAtual"]
     );
     echo json_encode($output);
 }
-
-?>
